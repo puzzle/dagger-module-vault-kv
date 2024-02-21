e2f1cc2feb6112450d6b01e9076a741bdfb7cdd8
Christoph Raaflaub <raaflaub@puzzle.ch>
2024-02-21 13:47:31 +0100

    document module

--- vault-kv/README.md
+++ vault-kv/README.md
@@ -3,3 +3,9 @@
 Module Home: https://github.com/puzzle/dagger-module-vault-kv
 
 Provides Vault functionality as [Dagger Module](https://daggerverse.dev/).
+
+## Documentation
+
+The Vault commands (CLI) is used to communicate with Vault.
+
+Check the original documentation: https://developer.hashicorp.com/vault/docs/commands/
--- vault-kv/main.go
+++ vault-kv/main.go
@@ -1,3 +1,5 @@
+// Vault kv (Key-Value) secrets engine module 
+
 package main
 
 import (
@@ -5,24 +7,46 @@ import (
 )
 
 type VaultKv struct {
+	// Vault address
 	Address string
+	// Vault access token used for login
 	Token   string
+	// `kv get` command option: mount
 	Mount   string
+	// path / key referencing the K/V secret
 	Path    string
+	// `kv get` output option: field
 	Field   string
 }
 
-func (c *VaultKv) NewForAddress(address string) (*VaultKv, error) {
+// Configure Vault address to connect to
+func (c *VaultKv) NewForAddress(
+	// Vault address
+	address string,
+) (*VaultKv, error) {
 	c.Address = address
 	return c, nil
 }
 
-func (c *VaultKv) Login(token string) (*VaultKv, error) {
+// Configure access token to be used for Vault login
+func (c *VaultKv) Login(
+	// Vault access token used for login
+	token string,
+) (*VaultKv, error) {
 	c.Token = token
 	return c, nil
 }
 
-func (c *VaultKv) GetKV(ctx context.Context, mount string, path string, field string) (string, error) {
+// The `kv get` command retrieves the value from K/V secrets from Vault.
+func (c *VaultKv) GetKV(
+	ctx context.Context,
+	// The path where the KV backend is mounted
+	mount string,
+	// path / key referencing the K/V secret
+	path string,
+	// Print only the field with the given name
+	field string,
+) (string, error) {
 	return dag.Container().From("vault:1.13.3").
 		WithEnvVariable("VAULT_ADDR", c.Address).
 		WithEnvVariable("SKIP_SETCAP", "1").
